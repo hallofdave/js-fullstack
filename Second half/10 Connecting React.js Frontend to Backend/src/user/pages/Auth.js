e4874b60f430fa06280d0787b6b8a8539358c447
hallofdave <93009866+hallofdave@users.noreply.github.com>
2023-07-27 19:50:47 -0600

    155

--- Second half/10 Connecting React.js Frontend to Backend/src/user/pages/Auth.js
+++ Second half/10 Connecting React.js Frontend to Backend/src/user/pages/Auth.js
@@ -74,12 +74,11 @@ const Auth = () => {
                   "Content-Type": "application/json",
                }
             );
-
-            auth.login();
+            auth.login(responseData.user.id);
          } catch (err) {}
       } else {
          try {
-            await sendRequest(
+            const responseData = await sendRequest(
                "http://localhost:5000/api/users/signup",
                "POST",
                JSON.stringify({
@@ -91,7 +90,8 @@ const Auth = () => {
                   "Content-Type": "application/json",
                }
             );
-            auth.login();
+
+            auth.login(responseData.user.id);
          } catch (err) {}
       }
    };
