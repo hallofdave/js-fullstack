e694b93875d4836930758bba4d8bf00a0f5de309
hallofdave <93009866+hallofdave@users.noreply.github.com>
2023-07-27 19:14:29 -0600

    153

--- Second half/10 Connecting React.js Frontend to Backend/src/shared/hooks/http-hook.js
+++ Second half/10 Connecting React.js Frontend to Backend/src/shared/hooks/http-hook.js
@@ -9,26 +9,32 @@ export const useHttpClient = () => {
    const sendRequest = useCallback(
       async (url, method = "GET", body = null, headers = {}) => {
          setIsLoading(true);
-         const httpAbortCtrll = new AbortController();
-         activeHttpRequest.current.push(httpAbortCtrll);
+         const httpAbortCtrl = new AbortController();
+         activeHttpRequest.current.push(httpAbortCtrl);
 
          try {
             const response = await fetch(url, {
                method,
                body,
                headers,
-               signal: httpAbortCtrll.signal,
+               signal: httpAbortCtrl.signal,
             });
             const responseData = await response.json();
+
+            activeHttpRequest.current = activeHttpRequest.current.filter(
+               (reqCtrl) => reqCtrl !== httpAbortCtrl
+            );
+
             if (!response.ok) {
                throw new Error(responseData.message);
             }
-
+            setIsLoading(false);
             return responseData;
          } catch (err) {
             setError(err.message);
+            setIsLoading(false);
+            throw err;
          }
-         setIsLoading(false);
       },
       []
    );
